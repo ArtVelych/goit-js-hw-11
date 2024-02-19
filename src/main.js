ce0a387bb7fafc00c5a01a20479ffb84f82eadc1
ArtVelych <artemvelychko1999@gmail.com>
2024-02-19 04:17:06 +0100

    'update'

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// import * as pixabay from './js/pixabay-api';
+// import * as galleryToRender from './js/render-functions';
+
